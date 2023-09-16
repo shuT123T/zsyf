c96476e4e1f599deb91d44cb10c5b6eb2393ba38
TNT <2939798573@qq.com>
2023-09-16 14:15:57 +0800

    Changes

--- src/main/java/com/slj/ZsyfTakeOutApplication.java
+++ src/main/java/com/slj/ZsyfTakeOutApplication.java
@@ -1,11 +1,8 @@
 package com.slj;
 
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
@@ -17,7 +14,8 @@ public class ZsyfTakeOutApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZsyfTakeOutApplication.class, args);
         log.info("启动成功");
-         log.info("启动成功!!!!!@#@#$$#");
+         log.info("git测试");
+
     }
 
 }
